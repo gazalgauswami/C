d562fec4a49a773ed685c349bb296f1089186a81
GAZAL GAUSWAMI <gzlgauswami07@gmail.com>
2022-07-19 20:46:47 +0530

    o

---
+++ IFNDEF.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+#define max 100
+void main()
+{clrscr();
+#ifndef min
+	printf("\n min is not define");
+
+#define min 10
+
+#else
+	printf("\n min is %d",min);
+#endif
+
+getch();
+}
