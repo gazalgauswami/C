b55dfcd72db371636dd1dc25acbc7cbe3998099f
GAZAL GAUSWAMI <gzlgauswami07@gmail.com>
2022-07-19 20:46:47 +0530

    o

---
+++ HOD.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+int i;
+clrscr();
+
+printf("\n Hexa Octal Desimal");
+
+for(i=0;i<=15;i++);
+{
+printf("\n %x,%o,%d",i,i,i);
+}
+
+getch();
+
+}
