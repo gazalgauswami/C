89be6bd828005ade84dee33dbd26b69d0e04a039
GAZAL GAUSWAMI <gzlgauswami07@gmail.com>
2022-07-19 20:46:57 +0530

    o

---
+++ strlen.c
@@ -0,0 +1,16 @@
+
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+
+void main()
+{
+    char a[10];
+
+    gets(a);
+
+    strlen(a);
+    printf("\n Lenth of string is : ",strlen(a));
+
+    getch();
+    }
