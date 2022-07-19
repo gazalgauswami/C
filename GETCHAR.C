2161d3fae190f92ea35afbe15874498334ba90b8
GAZAL GAUSWAMI <gzlgauswami07@gmail.com>
2022-07-19 20:46:47 +0530

    o

---
+++ GETCHAR.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+char c;
+clrscr();
+ printf("\n enter Alphebet:");
+ c= getchar();
+ printf("\n Alphebet is :");
+ putchar(c);
+ getch();
+
+ }
