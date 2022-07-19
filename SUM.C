39be5b1bf12cd89e50cc6553932c3a82e5e7b08a
GAZAL GAUSWAMI <gzlgauswami07@gmail.com>
2022-07-19 20:46:53 +0530

    o

---
+++ SUM.C
@@ -0,0 +1,20 @@
+
+
+#include<stdio.h>
+#include<conio.h>
+void  main()
+{
+    int a,b,sum;
+
+    printf("\n Enter First Number : ");
+    scanf("%d",&a);
+
+    printf("\n Enter Second Number: ");
+    scanf("%d",&b);
+
+    sum=a+b;
+
+    printf("\n Sum of Two Number is: %d",sum);
+
+    getch();
+    }
