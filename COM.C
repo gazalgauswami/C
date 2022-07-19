5d47e8de4d44e32d8395d38ad659db739f6076dd
GAZAL GAUSWAMI <gzlgauswami07@gmail.com>
2022-07-19 20:46:44 +0530

    o

---
+++ COM.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+
+void main()
+{
+char a[10]="gazal";
+char b[10]="pavan";
+
+clrscr();
+
+strcmp(a,b);
+printf("\n Diffranse Is:%d",strcmp("gazal","pavan"));
+
+getch();
+
+}
