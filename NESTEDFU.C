1140454ff4809778b1a9672345e1790ae4ff860e
GAZAL GAUSWAMI <gzlgauswami07@gmail.com>
2022-07-19 20:46:49 +0530

    o

---
+++ NESTEDFU.C
@@ -0,0 +1,23 @@
+//nested function find larger no from three number
+
+
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+  void function1(void);//function prototype
+  clrscr();
+  function1();//function call
+  getch();
+}
+void function1(void)//function defination
+{
+  void function2(void);//function prototype
+  printf("\n Function1");
+  function2();//function call
+}
+void function2(void)//function defination
+{
+  printf("\n Function2");
+}
