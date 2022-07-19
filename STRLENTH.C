c00bb8b153ccc83aa050cdd82be72e9441f4fb3a
GAZAL GAUSWAMI <gzlgauswami07@gmail.com>
2022-07-19 20:46:52 +0530

    o

---
+++ STRLENTH.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+char a[10];
+clrscr();
+printf("\n Enter String: ",a);
+gets(a);
+strlen(a);
+
+printf("\n Lenth of String is : %d",strlen(a));
+
+getch();
+
+}
