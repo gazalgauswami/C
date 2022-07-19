e0cc98c1a6d4ad254c6e6224b8abefc38b9b6ec3
GAZAL GAUSWAMI <gzlgauswami07@gmail.com>
2022-07-19 20:46:53 +0530

    o

---
+++ SUMBYONE.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+
+int a,b,c;
+clrscr();
+
+printf("\n Enter value of a:");
+scanf("%d",&a);
+
+printf("\n Enter value of b:");
+scanf("%d",&b);
+
+c=a+b;
+printf("\n a+b=%d",c);
+
+getch();
+}
