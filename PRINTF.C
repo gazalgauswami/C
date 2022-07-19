d05953e081aad72fed26e2ed46b70082fe68600f
GAZAL GAUSWAMI <gzlgauswami07@gmail.com>
2022-07-19 20:46:50 +0530

    o

---
+++ PRINTF.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+int a=5;
+float b=6.7;
+char c='g',d[10]="c langauge";
+clrscr();
+
+printf("\n \ta=%d,\tb=%f,\tc=%c,\td=%s",a,b,c,d);
+
+getch();
+}
