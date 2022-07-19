d03b094b3762e756b6d42d18e882b5c8886b2bb8
GAZAL GAUSWAMI <gzlgauswami07@gmail.com>
2022-07-19 20:46:52 +0530

    o

---
+++ STRCMP.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+
+void main()
+{
+char a[10];
+char b[15];
+clrscr();
+
+strcmp("gazal","mira");
+printf("\n Diffrance is : %d",strcmp("gazal","pira"));
+
+getch();
+}
