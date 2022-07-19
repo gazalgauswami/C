78f0dddcb608ad7884b1ce9038c059db9094ae60
GAZAL GAUSWAMI <gzlgauswami07@gmail.com>
2022-07-19 20:40:34 +0530

    s

---
+++ strlen2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+
+void main()
+{
+    char a[10];
+
+    gets("gazal");
+
+    strlen('gazal');
+    printf("\n Lenth of string is :%d ",strlen('gazal'));
+
+    getch();
+    }
