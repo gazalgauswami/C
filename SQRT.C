1dd7c0c1516fe6f9c6f2eb662a421fd6460a3455
GAZAL GAUSWAMI <gzlgauswami07@gmail.com>
2022-07-19 20:46:51 +0530

    o

---
+++ SQRT.C
@@ -0,0 +1,16 @@
+// write a programme to find squre root of number
+
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+
+void main()
+{
+int a=9;
+clrscr();
+sqrt(a);
+
+printf("\n sqrt is : %f",sqrt(a));
+getch();
+
+}
